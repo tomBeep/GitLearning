c4fab8361df79baaea2203a001da034982c7363f
Tom Edwards <tom@geneious.com>
2025-02-17 11:41:26 +1300

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String toPrint = "This is text that is being printed in a random format";
 
+        // This array should contain all available printers.
         Printer[] printers = new Printer[]{
-                new CamelCasePrinter()
+                new CamelCasePrinter() // FIX ME: why is there only 1 printer??
         };
 
         Printer randomPrinter = printers[(int) (Math.random() * printers.length)];
